26d9346fbc3fdd43c2e187bc2f3bc089a7ca101f
Pablo Jonhatan Solis Mendiola <pablo.solis@neoris.com>
2023-11-14 10:25:42 -0600

    Primer Commit

---
+++ src/app/clientes/cliente.ts
@@ -0,0 +1,16 @@
+export class Cliente {
+    id: number;
+    nombre: string;
+    apellido: string;
+    email: string;
+    createdAt: string;
+
+    constructor(id: number, nombre: string, apellido: string, email: string, createdAt: string) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+}
+
